71d708b5e07b3fb2b5ac9b336f9c5f2f003c65f3
Volker373 <swoodbury@yahoo.com>
2015-09-25 20:37:49 -0700

    rick Jmae

--- hue.cpp
+++ hue.cpp
@@ -3,4 +3,6 @@
 int main()
 {
 	std::cout << "HUEHUEHUEHUEHUE" << std::endl;
+	std::cout << "Yo, just do use namespace std; at top, it'll erase the need"
+			  << "For std::in front of all stream and string objects";
 }
